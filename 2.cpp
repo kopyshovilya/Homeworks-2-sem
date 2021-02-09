f3436b284bf64019467130088d9db742c0bd8342
kopyshovilya <kopyshov.io@phystech.edu>
2021-02-09 22:05:52 +0300

    Task 2

---
+++ 2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+
+#define maxCountStudents 15
+
+class Course{
+public:
+    bool addStudent(std::string nameStudent)
+    {
+        if(students.size() == maxCountStudents)
+            return 0;
+        students.push_back(nameStudent);
+        return 1;
+    }
+    bool delStudent(std::string nameStudent)
+    {
+        students.erase(std::find(students.begin(), students.end(), nameStudent));
+        return 1;
+    }
+    int count(){
+        return students.size();
+    }
+    std::string namePrepod;
+    void printListOfCourse(){
+        std::cout << "Teacher: " << namePrepod <<'\n';
+        std::cout << "Students:" <<'\n';
+        for(auto i:students)
+            std::cout << i <<'\n';
+    }
+private:
+    std::vector <std::string> students;
+
+};
+
+
+int main()
+{
+    return 0;
+}
